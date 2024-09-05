0259b3688ab506455465fe5657b5c3155030281b
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-09-05 22:24:54 +0100

    style: added color to Contact Me

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -6,18 +6,45 @@ const ContactMe = () => {
   return (
     <Container>
       <Title>Contact Me</Title>
-      <p>Email: <a href={`mailto:${emailAddress}`}>{emailAddress}</a></p>
+      <ContactInfo>
+      <MyEmail>Email: <EmailLink href={`mailto:${emailAddress}`}>{emailAddress}</EmailLink></MyEmail>
+      </ContactInfo>
     </Container>
   );
 };
 
 const Container = styled.div`
   padding: 2rem;
+  background: linear-gradient(135deg, #1411cc 10%, #bd10a0 100%);
+  justify-content: center;
+  
 `;
 
 const Title = styled.h1`
-  color: #ffffff;
+  color: #070707;
   text-align: center;
+  min-height: 15vh;
+  font-family: Georgia, "Times New Roman", Times, serif;
+`;
+
+const ContactInfo = styled.div`
+  
+`;
+
+const MyEmail = styled.p`
+  text-align: center;
+  color: white;
+  font-size: 25px;
+`;
+
+const EmailLink = styled.a`
+color: #1e87e9;
+font-size: 22px;
+text-decoration:none;
+
+&:hover {
+  text-decoration: underline
+}
 `;
 
 export default ContactMe;
